c6e9b412c8437a9a2f28336de543f8f1087761dd
Lunairi <mizukori250@gmail.com>
2017-11-23 12:41:44 -0800

    Restarting on LS, went about it the wrong way since I need to do some more pre-req with stat() and other function

--- main.c
+++ main.c
@@ -16,54 +16,53 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return (0);
 }
 
-int		ls(char *str, char *find, int fprint)
-{
-	struct dirent	*d;
-	DIR				*dir;
-
-	if ((dir = opendir(str)) == NULL)
-	{
-		dir = opendir(".");
-		if (dir == NULL)
-			return (0);
-	}
-	while ((d = readdir(dir)))
-	{
-		// printf("find %s str %s\n", find, d->d_name);
-		if(find == NULL && d->d_name[0] != '.')
-			printf("%s\n", d->d_name);
-		else if ((ft_strcmp(d->d_name, find)) && d->d_name[0] != '.')
-		{
-			printf("%s\n", d->d_name);
-			fprint = 1;
-		}
-	}
-	if (fprint == 0 && find != NULL)
-		printf("ft_ls: cannot access '%s': No such file or directory\n", find);
-}
-
-// int		simple_ls(char *str)
+// int		ls(char *str, char *find, int fprint)
 // {
 // 	struct dirent	*d;
 // 	DIR				*dir;
-// 	char			*cmp;
 
-// 	dir = opendir(str);
-// 	if (dir == NULL)
-// 		return (0);
+// 	if ((dir = opendir(".")) == NULL)
+// 	{
+// 			return (0);
+// 	}
 // 	while ((d = readdir(dir)))
 // 	{
-// 		cmp = d->d_name;
-// 		if(cmp[0] != '.' && cmp[0] != '\0')
-// 			printf("%s\n", cmp);
+// 		// printf("find %s str %s\n", find, d->d_name);
+// 		if(find == NULL && d->d_name[0] != '.')
+// 			printf("%s\n", d->d_name);
+// 		else if ((ft_strcmp(d->d_name, find)) && d->d_name[0] != '.')
+// 		{
+// 			printf("%s\n", d->d_name);
+// 			fprint = 1;
+// 		}
 // 	}
+// 	if (fprint == 0 && find != NULL)
+// 		printf("ft_ls: cannot access '%s': No such file or directory\n", find);
+// 	closedir(dir);
 // }
+
+int		ls(char *str)
+{
+	struct dirent	*d;
+	DIR				*dir;
+	char			*cmp;
+
+	dir = opendir(str);
+	if (dir == NULL)
+		return (0);
+	while ((d = readdir(dir)))
+	{
+		cmp = d->d_name;
+		if(cmp[0] != '.' && cmp[0] != '\0')
+			printf("%s\n", cmp);
+	}
+}
 
 void	parse_flag(char *str, char *find)
 {
-	str[0] == '-' && str[1] == '1' ? ls(str, find, 0) : 0;
-	str[0] == '-' && str[1] == 'R' ? ls(str, find, 0) : 0;
-	str[0] != '-' ? ls(str, find, 0) : 0;
+	(void)find;
+	str[0] == '-' && str[1] == '1' ? ls(str) : 0;
+	str[0] != '-' ? ls(str) : 0;
 }
 
 int		main(int ac, char **av)
