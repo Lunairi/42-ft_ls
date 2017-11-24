aad477f7ff521d6b2765288d66d472e742561aec
Lunairi <mizukori250@gmail.com>
2017-11-24 12:57:46 -0800

    Almost working sort algorithm, need to sort capital after lowercase

--- main.c
+++ main.c
@@ -4,16 +4,23 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int	ft_tolower(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (c + 32);
+	else
+		return (c);
+}
+
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
-	{
-		s1++;
-		s2++;
-	}
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	return (0);
+	int i;
+
+	i = 0;
+	while (ft_tolower(s1[i]) == ft_tolower(s2[i])
+			&& s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (ft_tolower(s1[i]) - ft_tolower(s2[i]));
 }
 
 // int		ls(char *str, char *find, int fprint)
@@ -73,32 +80,103 @@ char	*ft_strcpy(char *s1, const char *s2)
 	return (s1);
 }
 
-int		ls(char *str)
+int	ft_islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+int	ft_isalpha(int c)
+{
+	return (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'));
+}
+
+int	ft_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+int	ft_isalnum(int c)
+{
+	return (ft_isdigit(c) || ft_isalpha(c));
+}
+
+int		sort_list(char *one, char *two)
+{
+	return (ft_strcmp(one, two));
+}
+
+void	printsort(char **list, int size)
+{
+	int 	i;
+	char 	*tmp;
+	int		count;
+
+	i = 0;
+	count = 0;
+	// tmp = (char*)ft_memalloc(sizeof(char*)*99999);
+	while (i < size)
+	{
+		// printf("i - %i || size - %i\n", i, size);
+		if ((list[i + 1] != NULL))
+		{
+			if(ft_strcmp(list[i + 1], list[i]) < 0)
+			{
+				// printf("%d for %s and %s\n", ft_strcmp(list[i + 1], list[i]), list[i + 1], list[i]);
+				// printf("%s %s\n", list[i + 1], list[i]);
+				tmp = list[i];
+				list[i] = list[i + 1];
+				list[i + 1] = tmp;
+				count = 1;
+			}
+			else
+				i++;
+		}
+		else
+			i++;
+	}
+	if (count == 0)
+	{
+		i = 0;
+		while (i < size)
+		{
+			if (list[i][0] != '.')
+				printf("%s\n", list[i]);
+			i++;
+		}
+	}
+	else
+		printsort(list, size);
+}
+
+int		ls_one(char *str)
 {
 	struct dirent	*d;
 	DIR				*dir;
 	char			**list;
+	int				i;
 
+	i = 0;
 	list = (char**)ft_memalloc(sizeof(char**)*99999);
 	dir = opendir(str);
 	if (dir == NULL)
 		return (0);
 	while ((d = readdir(dir)))
 	{
-		*list = (char*)ft_memalloc(sizeof(char*)*99999);
+		list[i] = (char*)ft_memalloc(sizeof(char*)*99999);
 		// list[i] = ft_strcpy(list[i], d->d_name);
-		*list = d->d_name;
-		printf("%s\n", *list);
-		*list++;
+		list[i] = d->d_name;
+		// printf("%s\n", *list);
+		i++;
 	}
+	printsort(list, i);
 }
 
 void	parse_flag(char *str, char *find)
 {
 	(void)find;
-	// str[0] == '-' && str[1] == '1' ? ls(str) : 0;
-	// str[0] != '-' ? ls(str) : 0;
-	ls(str);
+	str[0] == '-' && str[1] == '1' ? ls_one(str) : 0;
+	str[0] != '-' ? ls_one(str) : 0;
+	// ls(str);
 }
 
 int		main(int ac, char **av)
