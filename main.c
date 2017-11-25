620610d8ea6690acdf8ea31822b125e7f48b047d
Lunairi <mizukori250@gmail.com>
2017-11-24 18:14:45 -0800

    Added --help, also used a struct to help moderate what flags are being applied to print, will be adding multiple argv support soon

--- main.c
+++ main.c
@@ -4,6 +4,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+typedef struct	s_flags
+{
+	int 		r;
+	int			a;
+	int			l;
+	int			search;
+	int			t;
+}				t_flags;
+
 int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
@@ -15,7 +24,7 @@ int	ft_tolower(int c)
 int		ft_isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
+		return (1);	
 	else
 		return (0);
 }
@@ -232,7 +241,7 @@ void	print_a(char **list, int size)
 		print_a(list, size);
 }
 
-void	print_one(char **list, int size)
+void	print_one(char **list, int size, t_flags *toggle)
 {
 	int 	i;
 	char 	*tmp;
@@ -259,27 +268,37 @@ void	print_one(char **list, int size)
 	{
 		// For reversing . and ..
 		swap_item(&list[0], &list[1]);
-		i = 0;
+		if (toggle->r == 1)
+			i = size - 1;
+		else
+			i = 0;
 		while (i < size)
 		{
-			if (list[i][0] != '.')
+			if (list[i][0] != '.' && toggle->a == 0)
 				printf("%s\n", list[i]);
-			i++;
+			if (toggle->a == 1)
+				printf("%s\n", list[i]);
+			if (toggle->r == 1)
+				i--;
+			else
+				i++;
+			if (i == -1)
+				i = size;
 		}
 	}
 	else
-		print_one(list, size);
+		print_one(list, size, toggle);
 }
 
-void	print_sort(char **list, int size, int type)
-{
-	// this wont work, will have to use a toggle system and scan the entire argvs coming in
-	type == 1 ? print_one(list, size) : 0;
-	type == 2 ? print_r(list, size) : 0;
-	type == 3 ? print_a(list, size) : 0;
-}
+// void	print_sort(char **list, int size, int type)
+// {
+// 	// this wont work, will have to use a toggle system and scan the entire argvs coming in
+// 	type == 1 ? print_one(list, size) : 0;
+// 	type == 2 ? print_r(list, size) : 0;
+// 	type == 3 ? print_a(list, size) : 0;
+// }
 
-int		ls_single(char *str, int type)
+int		ls_single(char *str, t_flags *toggle)
 {
 	struct dirent	*d;
 	DIR				*dir;
@@ -290,7 +309,11 @@ int		ls_single(char *str, int type)
 	list = (char**)ft_memalloc(sizeof(char**)*99999);
 	dir = opendir(str);
 	if (dir == NULL)
+	{
+		// need to discern between dir and file for display or error, will do later
+		printf("ft_ls: cannot access '%s': No such file or directory\n", str);
 		return (0);
+	}
 	while ((d = readdir(dir)))
 	{
 		list[i] = (char*)ft_memalloc(sizeof(char*)*99999);
@@ -299,30 +322,74 @@ int		ls_single(char *str, int type)
 		// printf("%s\n", *list);
 		i++;
 	}
-	print_sort(list, i, type);
+	print_one(list, i, toggle);
 	free(list);
 }
 
-void	parse_single(char *flag, char *search)
+int		parse_single(char *flag, char *search)
 {
+	t_flags *toggle;
+
+	if(flag[0] == '-')
+	{
+		if (flag[1] != 'r' && flag[1] != 'a' && flag[1] != '1')	
+		{
+			printf("ls: invalid option -- '%c'\n", flag[1]);
+			printf("Try 'ft_ls --help' for more information.\n");
+			return (0);
+		}
+	}
+	toggle = ft_memalloc(sizeof(t_flags));
 	// printf("flag %s, search %s", flag, search);
-	flag[0] == '-' && flag[1] == '1' ? ls_single(search, 1) : 0;
-	flag[0] == '-' && flag[1] == 'r' ? ls_single(search, 2) : 0;
-	flag[0] == '-' && flag[1] == 'a' ? ls_single(search, 3) : 0;
-	flag[0] != '-' ? ls_single(search, 1) : 0;
+	flag[0] == '-' && flag[1] == 'r' ? toggle->r = 1 : 0;
+	flag[0] == '-' && flag[1] == 'a' ? toggle->a = 1 : 0;
+	ls_single(search, toggle);
 	// ls(str);
+}
+
+void	parse_multi(char*flag, char *searc)
+{
+	printf("Currently in test");
+}
+
+void	help_ls()
+{
+	printf("Usage: ft_ls [OPTION]... [FILE]...\n");
+	printf("List information about the FILEs (the current directory by default).\n");
+	printf("Mandatory arguments to long options are mandatory for short options too.\n");
+  	printf("-a, --all                  do not ignore entries starting with .\n");
+	printf("-l                         use a long listing format\n");
+	printf("-r, --reverse              reverse order while sorting\n");
+	printf("-R, --recursive            list subdirectories recursively\n");
+	printf("-t                         sort by modification time, newest first\n");
+	printf("-1                         list one file per line\n");
+	printf("--help                     display this help and exit\n");
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
 
 int		main(int ac, char **av)
 {
-	if(ac < 4)
+	(void)ac;
+	if (av[1] == NULL)
+		parse_single(".", ".");
+	else if (!(ft_strcmp(av[1], "--help")))
+		help_ls();
+	else if (av[2] == NULL && av[1][0] == '-')
+		parse_single(av[1], ".");
+	else if (av[2] == NULL)
+		parse_single(".", av[1]);
+	else
 	{
-		if (av[1] == NULL)
-			parse_single(".", ".");
-		else if (av[2] == NULL)
-			parse_single(av[1], ".");
-		else
-			parse_single(av[1], av[2]);
+		parse_multi(av[1], av[2]);
 	}
 	// else if (ac >= 3)
 	// {
